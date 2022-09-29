43c5feb7750a21be69ada317af64ddd79b071e5e
Pushkar917 <pshkranand40@gmail.com>
2022-09-29 12:53:28 +0530

    Changes for User models

--- real_state/settings/base.py
+++ real_state/settings/base.py
@@ -23,7 +23,7 @@ env = environ.Env(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 environ.Env.read_env(BASE_DIR / ".env")
 
@@ -97,15 +97,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'real_state.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+AUTH_USER_MODEL = "users.User"
 
 
 # Password validation
@@ -142,9 +134,67 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIR = []
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import logging
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO"
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+
+        #formatters configuration
+
+        "formatters":{
+            "console":{
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s" 
+            },
+            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+
+        },
+
+
+        #handlers configuration
+
+        "handlers":{
+            "console":{
+                "class": "logging.StreamHandler",
+                "formatter": "console"
+            },
+
+            "file":{
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "formatter": "file",
+                "filename": "logs/real_estate.log",
+            },
+             "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+        },
+
+
+        #loggers configuration
+
+
+        "loggers": {
+            "": {"level":"INFO", "handlers": ["console", "file"], "propagate": False},
+            "apps": {"level":"INFO", "handlers": ["console", "file"], "propagate": False},
+            "django.server": DEFAULT_LOGGING["loggers"] ["django.server"],
+
+        }
+    }
+)
